376856600ec8e4e29bc17a37b767a3c7436e78b1
Sean Corfield <sean@corfield.org>
2019-03-31 17:29:21 -0700

    Add quoted entity functions

---
+++ src/next/jdbc/quoted.clj
@@ -0,0 +1,16 @@
+;; copyright (c) 2019 Sean Corfield, all rights reserved
+
+(ns next.jdbc.quoted
+  "Provides functions for use with the :entities option that define
+  how SQL entities should be quoted in strings constructed from
+  Clojure data.")
+
+(defn ansi "ANSI \"quoting\"" [s] (str \" s \"))
+
+(defn mysql "MySQL `quoting`" [s] (str \` s \`))
+
+(defn sql-server "SQL Server [quoting]" [s] (str \[ s \]))
+
+(def oracle "Oracle \"quoting\" (ANSI)" ansi)
+
+(def postgres "PostgreSQL \"quoting\" (ANSI)" ansi)
