67a2d3918c34f772e41e2928b97bd64647360a9d
Sean Corfield <sean@corfield.org>
2019-04-18 18:30:38 -0700

    Basic quoted tests

--- test/next/jdbc/quoted_test.clj
+++ test/next/jdbc/quoted_test.clj
@@ -1,5 +1,13 @@
 ;; copyright (c) 2019 Sean Corfield, all rights reserved
 
 (ns next.jdbc.quoted-test
-  (:require [clojure.test :refer [deftest is testing]]
+  (:require [clojure.test :refer [deftest are]]
             [next.jdbc.quoted :refer :all]))
+
+(deftest basic-quoting
+  (are [quote-fn quoted] (= (quote-fn "x") quoted)
+    ansi       "\"x\""
+    mysql      "`x`"
+    sql-server "[x]"
+    oracle     "\"x\""
+    postgres   "\"x\""))
