792bf242d70179a2fe6a7b6c9d5249e3ef22a20a
Sean Corfield <sean@corfield.org>
2019-05-26 16:15:33 -0700

    Add a stub test namespace for specs
    Just for consistency and completion.

---
+++ test/next/jdbc/specs_test.clj
@@ -0,0 +1,9 @@
+;; copyright (c) 2019 Sean Corfield, all rights reserved
+
+(ns next.jdbc.specs-test
+  "Stub test namespace for the specs.
+
+  The specs are used (and 'tested') as part of the tests for the
+  next.jdbc and next.jdbc.sql namespaces."
+  (:require [clojure.test :refer [deftest is testing]]
+            [next.jdbc.specs :refer :all]))
