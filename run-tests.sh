6c8c4528d4133b9da92b65e01b95e8d88b2cdcd1
Sean Corfield <sean@corfield.org>
2020-09-11 20:50:45 -0700

    Add my test runner script

---
+++ run-tests.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if test "$1" = "run"
+then
+	docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=Str0ngP4ssw0rd \
+		-p 1433:1433 --name sql-server19 \
+		-d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+fi
+if test "$1" = "rm"
+then
+	docker container rm sql-server19
+fi
+if test "$1" = "start"
+then
+	docker container start sql-server19
+fi
+if test "$1" = "stop"
+then
+	docker container stop sql-server19
+fi
+if test "$1" = "create"
+then
+	# assumes you already have a MySQL instance running locally
+	NEXT_JDBC_TEST_MYSQL=yes clojure -X:test next.jdbc.test-fixtures/create-clojure-test
+fi
+if test "$1" = ""
+then
+	NEXT_JDBC_TEST_MSSQL=yes MSSQL_SA_PASSWORD=Str0ngP4ssw0rd \
+		NEXT_JDBC_TEST_MYSQL=yes clojure -A:test:runner
+fi
