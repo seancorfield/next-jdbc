53a968863962f1ef11d3feacb32e698d73ec6adb
Sean Corfield <sean@corfield.org>
2021-08-02 20:23:24 -0700

    Setup build.clj for CI-like workflow

--- build.clj
+++ build.clj
@@ -1,10 +1,39 @@
 (ns build
   (:require [clojure.tools.build.api :as b]))
 
+(def lib 'com.github.seancorfield/next.jdbc)
+(def version (format "1.2.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (println "Cleaning target...")
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (println "Writing pom.xml...")
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (println "Copying src...")
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (println (str "Building jar " jar-file "..."))
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
 (defn run-tests
   [_]
   (let [basis (b/create-basis {:aliases [:test]})
         cmds  (b/java-command {:basis     basis
                                :main      'clojure.main
-                               :main-args ["-m" "cognitect.test-runner"]})]
-    (b/process cmds)))
+                               :main-args ["-m" "cognitect.test-runner"]})
+        {:keys [exit]} (b/process cmds)]
+    (when-not (zero? exit)
+      (throw (ex-info "Tests failed" {})))))
+
+(defn ci [opts]
+  (-> opts (run-tests) (clean) (jar)))
