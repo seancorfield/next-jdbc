aadad07c59321637d06ed2d64623a9e1737c2928
Sean Corfield <sean@corfield.org>
2022-08-05 15:15:15 -0700

    switch to docker-compose for SQL Server
    simplifies run-tests.sh and will make it easier
    to spin up other testing databases in the future.

--- run-tests.sh
+++ run-tests.sh
@@ -1,23 +1,15 @@
 #!/bin/sh
 
-if test "$1" = "run"
-then
-	docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=Str0ngP4ssw0rd \
-		-p 1433:1433 --name sql-server19 \
-		-d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
-fi
-if test "$1" = "rm"
-then
-	docker container rm sql-server19
-fi
-if test "$1" = "start"
-then
-	docker container start sql-server19
-fi
-if test "$1" = "stop"
-then
-	docker container stop sql-server19
-fi
+# start databases with: docker-compose up
+# then: ./run-tests.sh create
+# - creates a new database in MySQL for running tests
+#
+# test against "all" databases with MySQL JDBC driver:
+# ./run-tests.sh
+#
+# test against "all" databases with MariaDB JDBC driver:
+# ./run-tests.sh maria
+
 if test "$1" = "create"
 then
 	# assumes you already have a MySQL instance running locally
