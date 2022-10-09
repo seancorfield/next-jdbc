334d295cc9123e09b47be702b5c0d8ee72115ffa
Sean Corfield <sean@corfield.org>
2022-10-09 16:55:12 -0700

    switch run tests to bb

---
+++ run-tests.clj
@@ -0,0 +1,14 @@
+#!/usr/bin/env bb
+
+(require '[babashka.process :as p])
+
+(let [maria? (= "maria" (first *command-line-args*))
+      env
+      (cond-> {"NEXT_JDBC_TEST_MSSQL" "yes"
+               "NEXT_JDBC_TEST_MYSQL" "yes"
+               "MSSQL_SA_PASSWORD"    "Str0ngP4ssw0rd"}
+        maria?
+        (assoc "NEXT_JDBC_TEST_MARIA" "yes"))]
+  (-> (p/shell {:extra-env env} "clojure" "-X:test")
+      :exit
+      (System/exit)))
