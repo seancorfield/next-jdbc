482b26ade7b9f5ec4b73b7da67e04fffac23e6e1
Sean Corfield <sean@corfield.org>
2023-01-23 22:57:42 -0800

    drop use of build-clj; update deps

--- build.clj
+++ build.clj
@@ -12,29 +12,55 @@
   clojure -A:deps -T:build help/doc"
   (:refer-clojure :exclude [test])
   (:require [clojure.tools.build.api :as b]
-            [org.corfield.build :as bb]))
+            [clojure.tools.deps :as t]
+            [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.github.seancorfield/next.jdbc)
 (defn- the-version [patch] (format "1.3.%s" patch))
 (def version (the-version (b/git-count-revs nil)))
 (def snapshot (the-version "999-SNAPSHOT"))
+(def class-dir "target/classes")
 
 (defn test "Run all the tests." [opts]
-  (reduce (fn [opts alias]
-            (bb/run-tests (assoc opts :aliases [alias])))
-          opts
-          [:1.10 :1.11 :master])
+  (doseq [alias [:1.10 :1.11 :master]]
+    (println "\nRunning tests for Clojure" (name alias))
+    (let [basis    (b/create-basis {:aliases [:test alias]})
+          combined (t/combine-aliases basis [:test alias])
+          cmds     (b/java-command
+                    {:basis basis
+                     :java-opts (:java-opts combined)
+                     :main      'clojure.main
+                     :main-args ["-m" "cognitect.test-runner"]})
+          {:keys [exit]} (b/process cmds)]
+      (when-not (zero? exit) (throw "Tests failed"))))
   opts)
 
+(defn- jar-opts [opts]
+  (let [version (if (:snapshot opts) snapshot version)]
+    (assoc opts
+           :lib lib :version version
+           :jar-file (format "target/%s-%s.jar" lib version)
+           :scm {:tag (str "v" version)}
+           :basis (b/create-basis {})
+           :class-dir class-dir
+           :target "target"
+           :src-dirs ["src"]
+           :src-pom "template/pom.xml")))
+
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
-  (-> opts
-      (assoc :lib lib :version (if (:snapshot opts) snapshot version))
-      (test)
-      (bb/clean)
-      (assoc :src-pom "template/pom.xml")
-      (bb/jar)))
+  (test opts)
+  (b/delete {:path "target"})
+  (let [opts (jar-opts opts)]
+    (println "\nWriting pom.xml...")
+    (b/write-pom opts)
+    (println "\nCopying source...")
+    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
+    (println "\nBuilding JAR...")
+    (b/jar opts))
+  opts)
 
 (defn deploy "Deploy the JAR to Clojars." [opts]
-  (-> opts
-      (assoc :lib lib :version (if (:snapshot opts) snapshot version))
-      (bb/deploy)))
+  (let [{:keys [jar-file] :as opts} (jar-opts opts)]
+    (dd/deploy {:installer :remote :artifact (b/resolve-path jar-file)
+                :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))}))
+  opts)
